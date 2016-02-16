e5cfefc31a71b8d4b38a18d6b6cbf8f6e6c3beea
Chukwuerika Dike <rikkydyke@yahoo.com>
2016-02-16 12:23:03 +0100

    First commit

---
+++ exercises/even_numbers.py
@@ -0,0 +1,10 @@
+def even_numbers(low, high):
+	if low < high:
+		return "The first argument is higher than the second."
+	if type(low) == int or type(high) == int:
+		return "Only integers are valid input"
+	results = []
+	for i in range(low, high + 1):
+		if i % 2 == 0:
+			results.append(i)
+	return results
